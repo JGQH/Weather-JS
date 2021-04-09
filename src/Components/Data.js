bcd6ebe91c8dfbb560719d6931bd8b20a2e88c93
Juan Quispe <jquispeh63@gmail.com>
2021-04-08 19:01:19 -0500

    Finishes styles of recieved data
    -Adds .data-collection to Data.js to visualize data stored in info
    -Adds .data-collection styles to data-visualizer.scss
    -Modifies index.scss to not use grid outside of bigger screens

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -7,7 +7,14 @@ const Data = ({coordinates}) => {
     function getInfo() {
         setLoading(true);
         setInfo({
-            "weather": "scattered clouds"
+            "location": "Shuzenji (JP)", //Location
+            "weather": "scattered clouds",
+            "temperature": 11.49, // °C
+            "pressure": 1017, // hPa
+            "wind_speed": 0.89, // m/s
+            "cloudiness": 45, // %
+            "rain": undefined, // mm
+            "snow": undefined // mm
         })
         //setLoading(true);
     }
@@ -29,7 +36,38 @@ const Data = ({coordinates}) => {
         </div>
         {info && (
         <div className="data-collection">
-            {info.weather}
+            <div className="collection">
+                <div className="field">Location:</div>
+                <div className="value">{info.location}</div>
+            </div>
+            <div className="collection">
+                <div className="field">Weather:</div>
+                <div className="value">{info.weather}</div>
+            </div>
+            <div className="collection">
+                <div className="field">Temperature:</div>
+                <div className="value">{info.temperature} °C</div>
+            </div>
+            <div className="collection">
+                <div className="field">Pressure:</div>
+                <div className="value">{info.pressure} hPa</div>
+            </div>
+            <div className="collection">
+                <div className="field">Wind Speed:</div>
+                <div className="value">{info.wind_speed} ㎧</div>
+            </div>
+            <div className="collection">
+                <div className="field">Cloud Speed: </div>
+                <div className="value">{info.cloudiness}%</div>
+            </div>
+            {info.rain && <div className="collection">
+                <div className="field">Rain (Last 3hr):</div>
+                <div className="value">{info.rain}</div>
+            </div>}
+            {info.snow && <div className="collection">
+                <div className="field">Snow (Last 3hr):</div>
+                <div className="value">{info.snow}</div>
+            </div>}
         </div>)}
     </>);
 }
